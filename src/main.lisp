aa293103b38fad5da8a53698bf0009a65f4db2f1
momozor <skelic3@gmail.com>
2020-02-08 19:25:57 -0500

    finally found the root instance of tk

--- src/main.lisp
+++ src/main.lisp
@@ -3,14 +3,17 @@
   (:export :main))
 (in-package :lidle)
 
+;;; Constants
+(defvar +TOPLEVEL-WIDTH+ 600)
+(defvar +TOPLEVEL-HEIGHT+ 400)
 
 (defun main ()
-  (setf *debug-tk* nil)
   (with-ltk ()
-    (let ((b (make-instance
-              'button
-              :text "Hello World!"
-              :command (lambda ()
-                         (format t "Hello World!~%")
-                         (setf *exit-mainloop* t)))))
-      (pack b))))
+    (wm-title *tk* "Common Lisp IDLE")
+    (set-geometry *tk* 600 400 0 0)
+    (let ((quit-button
+           (make-instance 'button
+                          :text "Quit"
+                          :command (lambda ()
+                                     (setf *exit-mainloop* t)))))
+      (pack quit-button))))
